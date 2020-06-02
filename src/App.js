3d4f4033f8af11342a963d7bd27cc3db1f9370fe
Hussnain Javed <hussnain.jav3d@gmail.com>
2020-06-02 22:56:45 +0500

    Update App.js
    App.js is updated

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+            Hussnain Javed
         </a>
       </header>
     </div>
